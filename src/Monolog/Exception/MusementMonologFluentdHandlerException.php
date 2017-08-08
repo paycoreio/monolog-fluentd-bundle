49c8c043cb500e36bb95a98a8ba4da11bd39236f
Emiliano Gabrielli <emiliano.gabrielli@musement.com>
2017-08-08 15:29:10 +0200

    fix missing header comment

--- src/Monolog/Exception/MusementMonologFluentdHandlerException.php
+++ src/Monolog/Exception/MusementMonologFluentdHandlerException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of "musement/monolog-fluentd-bundle".
+ *
+ * (c) Musement S.p.A. <oss@musement.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Musement\MonologFluentdBundle\Monolog\Exception;
 
 class MusementMonologFluentdHandlerException extends \Exception
